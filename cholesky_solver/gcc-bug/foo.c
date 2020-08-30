0b371fc0096546a3f2da219465285ca13336b7a7
Michael S <already5chosen@yahoo.com>
2020-08-30 21:58:32 +0300

    gcc code generation bug - simpler reproducer.

--- cholesky_solver/gcc-bug/foo.c
+++ cholesky_solver/gcc-bug/foo.c
@@ -1,20 +1,13 @@
 #include <complex.h>
 
-void foo(double complex *x, int N, const double complex* triang)
+void foo(double complex *x, int N, const double complex* tri)
 {
-  triang += N*(N+1)/2; // point past end
-  x += N;              // point past end
-  for (int rlen = 1; rlen <= N; ++rlen) {
-   triang -= rlen; // point to diag element
-   x -= 1;
-   double complex acc = x[0];
-   for (int c = 1; c < rlen; ++c)
-     acc -= x[c] * triang[c];
-   x[0] = acc * creal(triang[0]);
-   // double complex acc = x[0];
-   // for (int c = 1; c < rlen; ++c)
-     // acc -= x[c] * triang[c];
-   // x[0] = acc * creal(triang[0]);
+  for (int r = 0; r < N; ++r) {
+   double complex acc = x[r];
+   for (int c = 0; c < r; ++c)
+     acc -= x[c] * tri[c];
+   x[r] = acc;
+   tri += r;
   }
 }
 
